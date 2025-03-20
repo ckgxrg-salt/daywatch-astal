97bb9aa0b2af9641ae14f8bd3ba852d0b2e22b34
ckgxrg <ckgxrg@ckgxrg.io>
2025-03-20 20:58:08 +0800

    Dashboard: Lock workspace when not in focus mode

--- util/hyprland.ts
+++ util/hyprland.ts
@@ -13,13 +13,19 @@ export function hyprInit() {
 		if (monitor.get_name() == "DP-1") {
 			Dashboard();
 		}
-	})
+	});
 	hypr.connect("client-added", (_, client) => {
 		if (!focused.get() && client.get_monitor().get_name() == "DP-1" && !client.get_floating() && client.get_class() != "alacritty-cava") {
 			exec(["notify-send", "-i", "laptop", "Astal", "Switched to Focus Mode.\nUse the Return button above to restore the dashboard."])
 			focused.set(true);
 		}
-	})
+	});
+	hypr.connect("workspace-added", (_, workspace) => {
+		if (workspace.get_monitor().get_name() == "DP-1" && !focused.get()) {
+			exec(["notify-send", "-i", "laptop", "Astal", "Cannot create new Workspace.\nDashboard is locked when not in Focus Mode."])
+			workspace.move_to(hypr.get_monitor(0));
+		}
+	});
 }
 
 // Send all clients back to primary screen
@@ -30,7 +36,7 @@ export function sendBack() {
 		if (each.get_monitor().get_name() == "DP-1" && each.get_class() != "alacritty-cava") {
 			each.move_to(workspace);
 		}
-	})
+	});
 }
 
 // Switch the focus back to primary monitor before any task
